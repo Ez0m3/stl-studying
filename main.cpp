c90422a33d7ec787086a37c4f6abe28b12231652
Ez0m3 <3451494496@qq.com>
2025-12-16 21:48:09 +0800

    我提交了cpp的更改

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
-#include<string>
 #include<vector>
-void test()
-{
-    std::cout<<"测试文件"<<std::endl;
-}
+void test();
 int main()
 {
     test();
     return 0;
+}
+void test()
+{
+    std::vector<int> v;
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(9);
+    for(std::vector<int>::iterator it=v.begin();it<v.end();it++)
+    {
+        std::cout<<*it;
+    }
+    std::system("pause");
 }
