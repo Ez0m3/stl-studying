7511751cc14dbf817589e07b9bf328ea983095f2
Ez0m3 <3451494496@qq.com>
2025-12-17 10:44:23 +0800

    范围for,const别名防止拷贝和修改优化性能

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
 #include<vector>
-void test();
+#include<string>
+class person
+{
+    public:
+    person(std::string name,int age):p_name(name),p_age(age)
+    {}
+    std::string p_name;
+    int p_age;
+};
+void test()
+{
+    person p1("hwx",24);
+    person p2("cjw",23);
+    person p3("micu",1);
+    std::vector<person> pvector={p1,p2,p3};
+    for(const auto& p:pvector)
+    {
+        std::cout<<p.p_name<<p.p_age<<"\n";
+    }
+}
 int main()
 {
     test();
     return 0;
-}
-void test()
-{
-    std::vector<int> v;
-    v.push_back(3);
-    v.push_back(6);
-    v.push_back(9);
-    for(std::vector<int>::iterator it=v.begin();it<v.end();it++)
-    {
-        std::cout<<*it;
-    }
-    std::system("pause");
 }
