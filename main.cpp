1626d766a757387d7a357d50cac43dfa04c26f99
Ez0m3 <3451494496@qq.com>
2025-12-18 16:02:43 +0800

    异常语法测试

--- main.cpp
+++ main.cpp
@@ -1,24 +1,14 @@
 #include<iostream>
-#include<vector>
-#include<string>
-class person
-{
-    public:
-    person(std::string name,int age):p_name(name),p_age(age)
-    {}
-    std::string p_name;
-    int p_age;
-};
 void test()
 {
-    person p1("hwx",24);
-    person p2("cjw",23);
-    person p3("micu",1);
-    std::vector<person> pvector={p1,p2,p3};
-    for(const auto& p:pvector)
+    try
     {
-        std::cout<<p.p_name<<p.p_age<<"\n";
+        throw std::runtime_error("我肚子饿了");
     }
+    catch(const std::exception& e)
+    {
+        std::cout<<e.what();
+    }   
 }
 int main()
 {
