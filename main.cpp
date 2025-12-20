f32b662a4415d90f4fa4576d66d94eb1c3b2a3d8
Ez0m3 <3451494496@qq.com>
2025-12-20 22:18:01 +0800

    类初始化执行顺序1.声明初始化2.初始化列表初始化3.构造函数体

--- main.cpp
+++ main.cpp
@@ -4,58 +4,20 @@
 class person
 {
     private:
-    std::string name;
-    int age;
+    int x=1;
+    int y=2;
     public:
-    person(std::string n,int a):name(n),age(a)
+    person():x(3)
     {
-        std::cout<<"调用了俩构造"<<"\n";
+        x=4;
+        y=5;
     }
-    person(int a):person("int",a)
+    void showinfo()
     {
-        std::cout<<"调用了单参数int构造"<<"\n";
+        std::cout<<x<<"  "<<y<<"\n";
     }
-    person(std::string n):person(n,20)
-    {
-        std::cout<<"调用了单参数string构造"<<"\n";
-    }
-    person():person("wucan",12)
-    {
-        std::cout<<"调用了无参构造"<<"\n";    
-    }
-    person(const person& other):name(other.name),age(other.age)
-    {
- 
-        std::cout<<"触发了拷贝构造"<<"\n";
-    }
-    person(person&& other)noexcept
-    :name(std::move(other.name)),age(std::exchange(other.age,0))
-    {
-        std::cout<<"触发了移动构造"<<"\n";
-    }
-    person& operator=(person&& other)noexcept
-    {
-        std::cout<<"触发了移动赋值运算符"<<"\n";
-        if(this!=&other)
-        {
-            name=std::move(other.name);
-            age=std::exchange(other.age,0);
-        }
-        return *this;
-    }
-    void showinfo()const
-    {
-        std::cout<<name<<"  "<<age<<"\n";
-    }    
 };
 int main()
 {
-
-    person p4{21};
-    person p1{std::move(p4)};
-    person p2;
-    p2=std::move(p1);
-    p1.showinfo();
-    p4.showinfo();
-    p2.showinfo();
+    person().showinfo();
 }
