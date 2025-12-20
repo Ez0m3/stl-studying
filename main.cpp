da3795a552d4a56676672b320765c09843fc82c1
Ez0m3 <3451494496@qq.com>
2025-12-21 01:10:55 +0800

    lambda加mutable虽然传的是形参，但是却创造了另外一个副本，可以保存

--- main.cpp
+++ main.cpp
@@ -1,31 +1,14 @@
 #include <iostream>
 #include <string>
-#include <utility>
-template<typename T>
-void inner(T&& t)
-{
-    std::cout<<"调用了右值"<<t<<std::endl;
-}
-template<typename T>
-void inner(T& t)
-{
-    std::cout<<"调用了左值"<<t<<std::endl;
-}
-template<typename T>
-void outer(T&& t)
-{
-    inner(std::forward<T>(t));
-}
-class person
-{
-
-};
+//
 int main()
 {
-    outer(3);
-    int a=3;
-    outer(a);
-    int* b=&a;
-    outer(b);
-    outer(std::move(b));
+    int a=5;
+    auto func=[a](int b=10)mutable
+    {
+        a++;
+        std::cout<<a*b<<"\n";
+    };
+    func();
+    func();
 }
