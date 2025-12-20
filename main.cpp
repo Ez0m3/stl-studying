016484af25d9da4673468fdd37b900287d26fd94
Ez0m3 <3451494496@qq.com>
2025-12-21 00:37:01 +0800

    万能引用和完美转发测试

--- main.cpp
+++ main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <string>
-#include<utility>
+#include <utility>
+template<typename T>
+void inner(T&& t)
+{
+    std::cout<<"调用了右值"<<t<<std::endl;
+}
+template<typename T>
+void inner(T& t)
+{
+    std::cout<<"调用了左值"<<t<<std::endl;
+}
+template<typename T>
+void outer(T&& t)
+{
+    inner(std::forward<T>(t));
+}
 class person
 {
-    public:
-    int x;
-    int y=5;
-    person():x(3)
-    {
-        std::cout<<"x的地址是"<<&x<<std::endl;
-        std::cout<<"y的地址是"<<&y<<std::endl;
-        std::cout<<"this的地址是"<<this<<std::endl;
-    }
-    int* getads()
-    {
-        return &y;
-    }
+
 };
 int main()
 {
-    const int &a=person().x;
-    std::cout<<a<<std::endl;
-    std::cout<<&a<<std::endl;
-    int* b=person().getads();
-    *(b-3)=10;
-    std::cout<<a<<std::endl;
-    std::cout<<*b<<std::endl;
+    outer(3);
+    int a=3;
+    outer(a);
+    int* b=&a;
+    outer(b);
+    outer(std::move(b));
 }
