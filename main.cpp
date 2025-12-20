7136c1d5295f0a5ca4cb7bd6cffd5816eb85e676
Ez0m3 <3451494496@qq.com>
2025-12-20 12:26:44 +0800

    委托构造和初始化列表测试代码

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-void test()
+#include <iostream>
+#include <string>
+class person
 {
-    int count = 0;
-    auto sf=[count]()mutable
+    private:
+    std::string name;
+    int age;
+    public:
+    person(std::string n,int a):name(n),age(a)
     {
-        count++;
-        std::cout<<count;
-    };
-    sf();
-} 
+        std::cout<<"调用了俩构造";
+    }
+    person(int a):person("int",a)
+    {
+        std::cout<<"调用了单参数int构造";
+    }
+    person(std::string n):person(n,20)
+    {
+        std::cout<<"调用了单参数string构造";
+    }
+    person():person("wucan",12)
+    {
+        std::cout<<"调用了无参构造";    
+    }
+    void showinfo()
+    {
+        std::cout<<name<<"  "<<age<<"\n";
+    }    
+};
 int main()
 {
-    test();
-    test();
-    test();
-    return 0;   
+    person p1;
+    p1.showinfo();
+    person p2(33);
+    p2.showinfo();
+    person p3("ccc");
+    p3.showinfo();
+    person p4("hwx",24);
+    p4.showinfo();
 }
