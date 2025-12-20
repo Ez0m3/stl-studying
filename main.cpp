024164def0c1d80613159611fe10df0aa1a065d5
Ez0m3 <3451494496@qq.com>
2025-12-20 12:59:14 +0800

    移动构造和移动赋值的测试代码

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include<utility>
 class person
 {
     private:
@@ -8,33 +9,53 @@ class person
     public:
     person(std::string n,int a):name(n),age(a)
     {
-        std::cout<<"调用了俩构造";
+        std::cout<<"调用了俩构造"<<"\n";
     }
     person(int a):person("int",a)
     {
-        std::cout<<"调用了单参数int构造";
+        std::cout<<"调用了单参数int构造"<<"\n";
     }
     person(std::string n):person(n,20)
     {
-        std::cout<<"调用了单参数string构造";
+        std::cout<<"调用了单参数string构造"<<"\n";
     }
     person():person("wucan",12)
     {
-        std::cout<<"调用了无参构造";    
+        std::cout<<"调用了无参构造"<<"\n";    
     }
-    void showinfo()
+    person(const person& other):name(other.name),age(other.age)
+    {
+ 
+        std::cout<<"触发了拷贝构造"<<"\n";
+    }
+    person(person&& other)noexcept
+    :name(std::move(other.name)),age(std::exchange(other.age,0))
+    {
+        std::cout<<"触发了移动构造"<<"\n";
+    }
+    person& operator=(person&& other)noexcept
+    {
+        std::cout<<"触发了移动赋值运算符"<<"\n";
+        if(this!=&other)
+        {
+            name=std::move(other.name);
+            age=std::exchange(other.age,0);
+        }
+        return *this;
+    }
+    void showinfo()const
     {
         std::cout<<name<<"  "<<age<<"\n";
     }    
 };
 int main()
 {
-    person p1;
+
+    person p4{21};
+    person p1{std::move(p4)};
+    person p2;
+    p2=std::move(p1);
     p1.showinfo();
-    person p2(33);
-    p2.showinfo();
-    person p3("ccc");
-    p3.showinfo();
-    person p4("hwx",24);
     p4.showinfo();
+    p2.showinfo();
 }
