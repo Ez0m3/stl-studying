c1c2e32043eae08b9f452ff39c2350e129884297
Ez0m3 <3451494496@qq.com>
2025-12-23 01:16:35 +0800

    高精度减法未去先导0版

--- main.cpp
+++ main.cpp
@@ -1,60 +1,99 @@
 #include <iostream>
 #include <string>
+#include<cmath>
 using std::string;
 void reverse(string& s)
 {
-    for(size_t i=0;i<s.size()/2;i++)
+    int size=s.size();
+    for(size_t i=0;i<size/2;i++)
     {
         char temp=s[i];
-        s[i]=s[s.size()-i-1];
-        s[s.size()-i-1]=temp;
+        s[i]=s[size-i-1];
+        s[size-i-1]=temp;
     }
+}
+bool compare(const string& s1,const string& s2)
+{
+    if(s1.size()!=s2.size())return s1.size()>s2.size();
+    if(s1.size()==s2.size())
+    {
+        for(size_t i=0;i<s1.size();i++)
+        {
+            if(s1[i]!=s2[i])return s1[i]>s2[i];
+        }
+    }
+    return true;
 }
 string myadd(const string& s1,const string& s2)
 {
-    int size1=s1.size();
-    int size2=s2.size();
-    int maxsize=size1>size2?size1:size2;
+    int i=s1.size()-1;
+    int j=s2.size()-1;
     string sum;
-    int ret=0;
-    for(int i=0;i<=maxsize;i++)
+    int carry=0;
+    while(i>=0||j>=0||carry>0)
     {
-        if(i<size1)
+        if(i>=0)
         {
-            ret+=s1[size1-i-1]-'0';
+            carry+=s1[i--]-'0';
         }
-        if(i<size2)
+        if(j>=0)
         {
-            ret+=s2[size2-i-1]-'0';
+            carry+=s2[j--]-'0';
         }
-        if(i<maxsize||ret>0)
-        {
-            sum.push_back(ret%10+'0');
-        }
-        ret/=10;
+        sum.push_back(carry%10+'0');
+        carry/=10;
     }
     reverse(sum);
-    for(auto c:sum)
-    {
-        std::cout<<c;
-    }
-    std::cout<<"\n";
     return sum;
 }
-string mysub(const string& s1,const string& s2)
+string mysub(const string& a,const string& b)
 {
+    const string& s1 = compare(a, b) ? a : b; // s1 = max(a,b)
+    const string& s2 = compare(a, b) ? b : a; // s2 = min(a,b)
+    int i=s1.size()-1;
+    int j=s2.size()-1;
+    string diff;
+    int flag=0;
+    while(i>=0||j>=0)
+    {
+        int borrow=flag;
+        if(i>=0)
+        {
+            borrow+=s1[i--]-'0';
+        }
+        if(j>=0)
+        {
+            borrow-=(s2[j--]-'0');
+        }
+        flag=0;
+        if(borrow<0)
+        {
+            borrow+=10;
+            flag=-1;
+        }
+        diff.push_back(borrow+'0');
+    }
+    if(!compare(a,b))
+    {
 
+        diff.push_back('-');
+    }
+    reverse(diff);
+    return diff;
 }
 void menu()
 {
     string s1,s2;
     std::cout<<"请输入高精度整数"<<"\n";
     std::cin>>s1;
-
     std::cout<<"请输入高精度整数"<<"\n";
     std::cin>>s2;
-    string sum=myadd(s1,s2);
-    //string sub=mysub(s1,s2);
+    string sub=mysub(s1,s2);
+    for(auto c:sub)
+    {
+        std::cout<<c;
+    }
+    std::cout<<"\n";
 }
 int main()
 {
