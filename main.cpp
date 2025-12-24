f002455d32b30971d6b9dc9fc954c5a97cc2fb40
Ez0m3 <3451494496@qq.com>
2025-12-24 21:45:54 +0800

    插入排序以及std::moven完美场景

--- main.cpp
+++ main.cpp
@@ -3,29 +3,29 @@
 template<typename T,std::size_t N>
 void bubble_sort(T (&arr)[N])
 {
-    int size=static_cast<int>(N);
-    for(int i=0;i<size-1;++i)
+    const int n=static_cast<int>(N);
+    for(int i=0;i<n-1;++i)
     {
-        bool isswap=false;
-        for(int j=0;j<size-i-1;++j)
+        bool swapped=false;
+        for(int j=0;j<n-i-1;++j)
         {
             if(arr[j]>arr[j+1])
             {
                 std::swap(arr[j],arr[j+1]);
-                isswap=true;
+                swapped=true;
             }
         }
-        if(!isswap)return;
+        if(!swapped)return;
     }
 }
 template<typename T,std::size_t N>
 void selection_sort(T (&arr)[N])
 {
-    int size=static_cast<int>(N);
-    for(int i=0;i<size-1;++i)
+    const int n=static_cast<int>(N);
+    for(int i=0;i<n-1;++i)
     {
         int minindex=i;
-        for(int j=i+1;j<size;++j)
+        for(int j=i+1;j<n;++j)
         {
             if(arr[j]<arr[minindex])
             minindex=j;
@@ -36,10 +36,26 @@ void selection_sort(T (&arr)[N])
         }
     }
 }
+template<typename T,std::size_t N>
+void insertion_sort(T (&arr)[N])
+{
+    const int n=static_cast<int>(N);
+    for(int i=1;i<n;++i)
+    {
+        T temp=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>temp)
+        {
+            arr[j+1]=std::move(arr[j]);
+            --j;
+        }
+        arr[j+1]=temp;
+    }
+}
 int main()
 {
-    int arr[]{3,5,1,65,87,2,534};
-    selection_sort(arr);
+    int arr[]{142,7,23,99,3,5,1,65,0,7,2,534,11};
+    insertion_sort(arr);
     for(auto x:arr)
     {
         std::cout<<x<<" ";
