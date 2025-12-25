0ffaf061beefc56928a42743f4a5c43df161555a
Ez0m3 <3451494496@qq.com>
2025-12-25 12:01:17 +0800

    计数排序

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ void shell_sort(T (&arr)[N])
     const int n=static_cast<int>(N);
     for(int gap=n/2;gap>0;gap/=2)
     {
-        for(int i=0;i<gap;i++)         //相同gap组距的不同组循环
+        for(int i=0;i<gap;++i)         //相同gap组距的不同组循环
         {
             for(int j=i+gap;j<n;j+=gap)   //同一个组的组内循环,起始arr[i]，每一次循环后有序区整体增加一个arr[j+gap]
             {
@@ -74,10 +74,36 @@ void shell_sort(T (&arr)[N])
         }
     }
 }
+template<typename T,std::size_t N>
+void counting_sort(T (&arr)[N])
+{
+    const int n=static_cast<int>(N);
+    int min{arr[0]};
+    int max{arr[0]};
+    for(int i=1;i<n;++i)
+    {
+        if(arr[i]>max)max=arr[i];
+        if(arr[i]<min)min=arr[i];
+    }
+    int count[max-min+1]{};
+    for(int i=0;i<n;++i)
+    {
+        ++count[arr[i]-min];
+    }
+    int index=0;
+    for(int i=0;i<=max-min;++i)
+    {
+        while(count[i]>0)
+        {
+            arr[index++]=min+i;
+            --count[i];
+        }
+    }
+}
 int main()
 {
     int arr[]{142,7,23,99,3,5,1,65,0,7,2,534,11};
-    shell_sort(arr);
+    counting_sort(arr);
     for(auto x:arr)
     {
         std::cout<<x<<" ";
