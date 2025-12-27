2d17db37d16ef5d525da68e915420b112b809b4b
Ez0m3 <3451494496@qq.com>
2025-12-28 00:27:26 +0800

    无哨兵无迭代器单向链表

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include <utility>
+#include<stdexcept>
+#include <cassert> 
 template<typename T>
 class LinkedList{
 private:
@@ -20,15 +23,31 @@ public:
     LinkedList(LinkedList&& other)noexcept;
     LinkedList& operator=(LinkedList&& other)noexcept;
     size_t get_size()const{return size;}
-    bool isempty()const{return size==0;}
+    bool isempty()const{return phead==nullptr;}
     void clear();
+    T& front(){
+        if(isempty())throw std::out_of_range("front() on empty list");
+        return phead->data;
+    }
+    const T& front()const{
+        if(isempty())throw std::out_of_range("front() on empty list");
+        return phead->data;
+    }
+    T& back(){
+        if(isempty())throw std::out_of_range("back() on empty list");
+        return ptail->data;
+    }
+    const T& back()const{
+        if(isempty())throw std::out_of_range("back() on empty list");
+        return ptail->data;
+    }
     void push_front(const T& value);
     void push_front(T&& value);
     void push_back(const T& value);
     void push_back(T&& value);
-    T& pop_front();  //增加返回值？
-    void pop_back();    //增加返回值？
-    int find_index(const T& value)const;
+    T pop_front();  
+    T pop_back();    
+    size_t find_index(const T& value)const;
     void print()const;
     void reverse();
 };
@@ -72,10 +91,8 @@ LinkedList<T>& LinkedList<T>::operator=(const LinkedList& other){
 }
 
 template<typename T>
-LinkedList<T>::LinkedList(LinkedList&& other)noexcept{
-    phead=other.phead;
-    ptail=other.ptail;
-    size=other.size;
+LinkedList<T>::LinkedList(LinkedList&& other)noexcept
+    :phead(other.phead),ptail(other.ptail),size(other.size){
     other.phead=nullptr;
     other.ptail=nullptr;
     other.size=0;  
@@ -87,7 +104,10 @@ LinkedList<T>& LinkedList<T>::operator=(LinkedList&& other)noexcept{
     phead=other.phead;
     ptail=other.ptail;
     size=other.size;
-    other.clear();
+    other.phead=nullptr;
+    other.ptail=nullptr;
+    other.size=0;
+    return *this;
 }
 
 template<typename T>
@@ -138,19 +158,50 @@ void LinkedList<T>::push_back(T&& value){
 }
 
 template<typename T>
-T& LinkedList<T>::pop_front(){
+T LinkedList<T>::pop_front(){
     if(phead==nullptr){
         throw std::out_of_range("pop_front on empty list");
     }else{
         Node* ptemp=phead->next;
-        T temp=std::move(phead->data);
+        T value=std::move(phead->data);
         delete phead;
         phead=ptemp;
         if(phead==nullptr)ptail=nullptr;
         --size;
-        T& value=temp;
         return value;
     }
+}
+
+template<typename T>
+T LinkedList<T>::pop_back(){
+    if(phead==nullptr){
+        throw std::out_of_range("pop_back on empty list");
+    }else if(phead==ptail){
+        T value=std::move(phead->data);
+        delete phead;
+        phead=ptail=nullptr;
+        --size;
+        return value;
+    }else{
+        Node* ptemp=phead;
+        while(ptemp->next!=ptail)ptemp=ptemp->next;
+        T value=std::move(ptail->data);
+        delete ptail;
+        ptail=ptemp;
+        ptail->next=nullptr;
+        --size;
+        return value;
+    }
+}
+
+template<typename T>
+size_t LinkedList<T>::find_index(const T& value)const{
+    size_t index=0;
+    for(Node* ptemp=phead;ptemp!=nullptr;ptemp=ptemp->next,++index){
+        if(ptemp->data==value)return index;
+    }
+    std::cout<<"未找到值为"<<value<<"的元素,返回88888888作为记号"<<"\n";
+    return 88888888;
 }
 
 template<typename T>
@@ -161,13 +212,23 @@ void LinkedList<T>::print()const{
     std::cout<<"\n";
 }
 
+template<typename T>
+void LinkedList<T>::reverse(){
+    if(phead==ptail)return;
+    Node* pprev=nullptr;
+    Node* pcurr=phead;
+    Node* pnext=pcurr;
+    while(pnext!=nullptr){
+        pnext=pcurr->next;
+        pcurr->next=pprev;
+        pprev=pcurr;
+        pcurr=pnext;
+    }
+    pnext=phead;
+    phead=ptail;
+    ptail=pnext;
+}
+
 int main(){
-    LinkedList<int> list;
-    list.push_front(10);
-    list.push_front(20);
-    list.push_front(30);
-    list.print();
-    auto& x=list.pop_front();
-    list.pop_front();
-    std::cout<<x;
+    
 }
