38df518923492e6e6b0ec6735d5e93ef553a481c
Ez0m3 <3451494496@qq.com>
2025-12-29 17:17:34 +0800

    智能指针

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,19 +1,15 @@
-#include<iostream>
-#include<memory>
-#include<string>
-using std::string;
-class man{
-public:
-    string m_name;
-    man(string name="xxx")
-        :m_name(name){
-        std::cout<<m_name<<"我构造了"<<"\n";
-    }
-    ~man(){
-        std::cout<<m_name<<"析构了"<<"\n";
-    }
+template<typename T>
+class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        std::unique_ptr<Node> next;  
+        Node* prev;              
+        Node(T value) : data(std::move(value)), next(nullptr), prev(nullptr) {}
+    };
+    std::unique_ptr<Node> head;  
+    Node* tail = nullptr;       
 };
 int main(){
-    std::unique_ptr<man> ptr1=std::make_unique<man>("hwx");
-    std::cout<<ptr1->m_name;
+
 }
