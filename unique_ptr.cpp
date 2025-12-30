90ea2bd59f4411f7d0e06010aa32084e0ee8d9cf
Ez0m3 <3451494496@qq.com>
2025-12-30 12:13:14 +0800

    单个堆区资源的所有权移动

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,15 +1,19 @@
-template<typename T>
-class DoublyLinkedList {
-private:
-    struct Node {
-        T data;
-        std::unique_ptr<Node> next;  
-        Node* prev;              
-        Node(T value) : data(std::move(value)), next(nullptr), prev(nullptr) {}
-    };
-    std::unique_ptr<Node> head;  
-    Node* tail = nullptr;       
+#include <iostream>
+#include <memory>
+using std::unique_ptr;
+class person{
+public:
+    person(){
+        std::cout<<"构造"<<std::endl;
+    }
+    ~person(){
+        std::cout<<"析构"<<std::endl;
+    }
 };
+unique_ptr<person> makeperson(){
+    return unique_ptr<person>(new person());
+}
 int main(){
-
+    unique_ptr<person> ptr1=makeperson();
+    unique_ptr<person> ptr2=std::move(ptr1);
 }
