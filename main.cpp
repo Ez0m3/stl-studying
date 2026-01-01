3071aa373ce12b2c5cf3eb53d4e8d24c31f30d85
Ez0m3 <3451494496@qq.com>
2026-01-01 22:12:05 +0800

    简单队列，无边界检查

--- main.cpp
+++ main.cpp
@@ -1,26 +1,43 @@
 #include<iostream>
 #include<memory>
+#include<string>
+#include<cassert>
 using std::unique_ptr;
-// template<typename T>
-// class deque{
-// private:
-//     struct node{
-//         T data;
-//         unique_ptr<node> pnext;
-//         node(T value=T(),unique_ptr<node> ptr)
-//             :data(value),pnext(ptr){}
-//     };
-// };
 template<typename T>
-struct node{
+class queue{
+private:
+    struct node{
         T data;
         unique_ptr<node> pnext;
         node(T value=T(),unique_ptr<node> ptr=nullptr)
-            :data(value),pnext(ptr){}
+            :data(value),pnext(std::move(ptr)){}
     };
-int main(){
-    node<int> node1(3);
-    node<int> node2(4);
-    node1=std::move(node2);
-    std::cout<<node2.data<<"\n";
+    unique_ptr<node> phead;
+    node* ptail;
+    int size;
+public:
+    queue()
+        :phead(std::make_unique<node>()),ptail(phead.get()),size(0){}
+    bool isempty()const{return phead->pnext==nullptr;}
+    void push(T value){
+        ptail->pnext=std::make_unique<node>(value);
+        ptail=ptail->pnext.get();
+        ++size;
+    }
+    T pop(){
+        if(isempty())throw std::out_of_range("empty queue");
+        T temp=std::move(phead->pnext->data);
+        phead->pnext=std::move(phead->pnext->pnext);
+        --size;
+        return temp;
+    }
+    T getfront()const{
+        return phead->pnext->data;
+    }
+    T getend()const{
+        return ptail->data;
+    }
+};
+int main() {
+  
 }
