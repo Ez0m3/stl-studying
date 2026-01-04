75fce1bc56aa45cedd3c801e27d2c910d2ceeb47
Ez0m3 <3451494496@qq.com>
2026-01-04 19:40:01 +0800

    盛水容器

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,38 +1,22 @@
 #include<iostream>
-#include<stack>
-class MyQueue {
-private:
-    std::stack<int> via;
-    std::stack<int> stk;
+#include<vector>
+#include<algorithm>
+class Solution {
 public:
-    MyQueue() {
-    
-    }
-    
-    void push(int x) {
-        stk.push(x);
-        while(!stk.empty()){
-            int temp=stk.top();
-            stk.pop();
-            via.push(temp);
+    int maxArea(std::vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int h=std::min(height[l],height[r]);
+        int area=h*(r-l);
+        while(l!=r){
+            height[l]<height[r]?++l:--r;
+            h=std::min(height[l],height[r]);
+            int newarea=h*(r-l);
+            area=std::max(newarea,area);
         }
-        via.swap(stk);
-    }
-    
-    int pop() {
-        int value=stk.top();
-        stk.pop();
-        return value;
-    }
-    
-    int peek() {
-        return stk.top();
-    }
-    
-    bool empty() {
-        return stk.empty();
+        return area;
     }
 };
 int main(){
-
+    
 }
