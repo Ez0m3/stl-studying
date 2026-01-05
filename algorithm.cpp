7216ff1e5d6448dfcbea3ba4ec746bc18165d0da
Ez0m3 <3451494496@qq.com>
2026-01-05 11:01:46 +0800

    morris中序遍历

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,22 +1,39 @@
-#include<iostream>
 #include<vector>
-#include<algorithm>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
 class Solution {
 public:
-    int maxArea(std::vector<int>& height) {
-        int l=0;
-        int r=height.size()-1;
-        int h=std::min(height[l],height[r]);
-        int area=h*(r-l);
-        while(l!=r){
-            height[l]<height[r]?++l:--r;
-            h=std::min(height[l],height[r]);
-            int newarea=h*(r-l);
-            area=std::max(newarea,area);
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> datas;
+        if(!root)return datas;
+        auto curr=root;
+        while(curr){
+            if(!curr->left){
+                datas.push_back(curr->val);
+                curr=curr->right;
+            }else{
+                auto prev=curr->left;
+                while(prev->right!=nullptr&&prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==curr){
+                    prev->right=nullptr;
+                    datas.push_back(curr->val);
+                    curr=curr->right;
+                }else{
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+            }
         }
-        return area;
+        return datas;
     }
 };
-int main(){
-    
-}
