a049e06e813e27dbd4ccaa020d0f095bff911e03
Ez0m3 <3451494496@qq.com>
2026-01-19 13:45:48 +0800

    并发编程学习

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include<vector>
-using namespace std;
-class Graph{
-    vector<vector<int>> g;
-    int n;
-public:
-    Graph(int n):n(n){
-        g.resize(n,vector<int>(n,0));
+#include<iostream>
+#include<thread>
+void print(int x){
+    for(int i=0;i<1000;++i){
+        if(x>=0){
+            std::cout<<x+i<<std::endl;
+        }else{
+            std::cout<<x-i<<std::endl;
+        }
     }
-    void add(int u,int v,int w){
-        g[u][v]=w;
-    }
-    void dfs(int u,vector<bool>& vis){
-        vis[u]=true;
-        for(int v=0;v<n;v++)
-            if(g[u][v]&&!vis[v]) dfs(v,vis);
-    }
-};
+}
+int main(){
+    std::thread thread1(print,1);
+    std::thread thread2(print,-1);
+    thread1.join();
+    thread2.join();
+    return 0;
+}
